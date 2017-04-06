0b201862a3d58aacbc9bfc67f6340e48b8fd196c
Tony Archer <tony@zeg.io>
2017-04-06 15:49:30 -0500

    Enable cell merging

--- index.js
+++ index.js
@@ -1,87 +1,95 @@
+const excel = require('./lib/excel')
 
-'use strict';
+let buildExport = params => {
+  if (!(params instanceof Array)) throw 'buildExport expects an array'
 
-const excel = require('./../../../../Downloads/node-excel-export-master/lib/excel');
-module.exports = {
-  buildExport: function(params) {
-    if( ! (params instanceof Array)) throw 'buildExport expects an array';
+  let sheets = []
+  params.forEach(function (sheet, index) {
+    let specification = sheet.specification
+    let dataset = sheet.data
+    let sheet_name = sheet.name || 'Sheet' + index + 1
+    let data = []
+    let merges = sheet.merges
+    let config = {
+      cols: []
+    }
 
-    let sheets = [];
-    params.forEach(function(sheet, index) {
-      let specification = sheet.specification;
-      let dataset = sheet.data;
-      let sheet_name = sheet.name || 'Sheet' + index+1;
-      let data = [];
-      let config = {
-        cols: []
-      };
+    if (!specification || !dataset) throw 'missing specification or dataset.'
 
-      if( ! specification || ! dataset) throw 'missing specification or dataset.';
+    if (sheet.heading) {
+      sheet.heading.forEach(function (row) {
+        data.push(row)
+      })
+    }
 
-      if(sheet.heading) {
-        sheet.heading.forEach(function(row) {
-          data.push(row);
-        });
-      }
+    //build the header row
+    let header = []
+    for (let col in specification) {
+      header.push({
+        value: specification[col].displayName,
+        style: specification[col].headerStyle || ''
+      })
 
-      //build the header row
-      let header = [];
-      for (let col in specification) {
-        header.push({
-          value: specification[col].displayName,
-          style: specification[col].headerStyle || ''
-        });
-
-        if(specification[col].width) {
-          if(Number.isInteger(specification[col].width)) config.cols.push({wpx: specification[col].width});
-          else if(Number.isInteger(parseInt(specification[col].width))) config.cols.push({wch: specification[col].width});
-          else throw 'Provide column width as a number';
+      if (specification[col].width) {
+        if (Number.isInteger(specification[col].width)) {
+          config.cols.push({ wpx: specification[col].width })
+        } else if (Number.isInteger(parseInt(specification[col].width))) {
+          config.cols.push({ wch: specification[col].width })
         } else {
-          config.cols.push({});
+          throw 'Provide column width as a number'
         }
-
+      } else {
+        config.cols.push({})
       }
-      data.push(header); //Inject the header at 0
 
-      dataset.forEach(record => {
-        let row = [];
-        for (let col in specification) {
-          let cell_value = record[col];
+    }
+    data.push(header) //Inject the header at 0
 
-          if(specification[col].cellFormat && typeof specification[col].cellFormat == 'function') {
-            cell_value = specification[col].cellFormat(record[col], record);
-          }
+    dataset.forEach(record => {
+      let row = []
+      for (let col in specification) {
+        let cell_value = record[col]
 
-          if(specification[col].cellStyle && typeof specification[col].cellStyle == 'function') {
-            cell_value = {
-              value: cell_value,
-              style: specification[col].cellStyle(record[col], record)
-            };
-          } else if(specification[col].cellStyle) {
-            cell_value = {
-              value: cell_value,
-              style: specification[col].cellStyle
-            };
-          }
-          
-          if (specification[col].mergeGroup) {
-                cell_value.merge = specification[col].mergeGroup;
-          }
-          
-          row.push(cell_value); // Push new cell to the row
+        if (specification[col].cellFormat && typeof specification[col].cellFormat == 'function') {
+          cell_value = specification[col].cellFormat(record[col], record)
         }
-        data.push(row); // Push new row to the sheet
-      });
 
-      sheets.push({
-        name: sheet_name,
-        data: data,
-        config: config
-      });
+        if (specification[col].cellStyle && typeof specification[col].cellStyle == 'function') {
+          cell_value = {
+            value: cell_value,
+            style: specification[col].cellStyle(record[col], record)
+          }
+        } else if (specification[col].cellStyle) {
+          cell_value = {
+            value: cell_value,
+            style: specification[col].cellStyle
+          }
+        }
 
-    });
 
-    return excel.build(sheets);
+        // TODO: REMOVE
+        // if (specification[col].mergeGroup) {
+        //   cell_value.merge = specification[col].mergeGroup
+        // }
 
-  }
+        row.push(cell_value) // Push new cell to the row
+      }
+      data.push(row) // Push new row to the sheet
+    })
+
+    sheets.push({
+      name: sheet_name,
+      data: data,
+      merge: merges,
+      config: config
+    })
+
+  })
+
+  return excel.build(sheets)
+
+}
+
+module.exports = {
+  buildExport
 }
