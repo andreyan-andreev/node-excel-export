cfa710888e41fef88d6eb89dd3de50051e3a38bb
Tony Archer <tony@zeg.io>
2017-04-06 13:49:31 -0500

    Remove node_modules from repo

--- node_modules/xlsx-style/example2.js
+++
@@ -1,3 +0,0 @@
-var XLSX = require('/Users/pieter/Dev/js-xlsx');
-var workbook = XLSX.readFile('/Users/pieter/Exp/js-xlsx/lab/date/date.xlsx', {type: "xlsx", cellDates: true, cellStyles: true});
-console.log(workbook.Sheets);
